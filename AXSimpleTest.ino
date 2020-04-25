de5ff3ca1ec65262d5955e857aaa6a6d8100e2c3
Adrian Acosta Mitjans <adrian.mitjans@gmail.com>
2020-04-25 14:30:23 -0700

    Varios cambios
    Se reimplementó toda la aplicación. Ahora se permite ir de abajo hacia arriba (y viceversa) o de derecha a izquierda (y viceversa) en un solo movimiento. El movimiento de centrar es mas natural y centra desde cualquier posición.

--- AXSimpleTest.ino
+++ AXSimpleTest.ino
@@ -2,13 +2,15 @@
 #include <ax12.h>
 
 char d = 'a';
+int s = 20;
+int engine1 = 512;
+int engine2 = 512;
 
 void setup()
 {
     Serial.begin(9600);
-    SetPosition(1,512); //set the position of servo # 1 to '0'
-    SetPosition(2,512);
-    //delay(10);//wait for servo to move
+    SetPosition(1,engine1);
+    SetPosition(2,engine2);
 }
 
 void loop()
@@ -17,33 +19,25 @@ void loop()
      // Get next command from Serial (add 1 for final 0)
      char c = Serial.read();
      if(c == 'n'){
-       //delay,iterations,center,up,down
-       nod(20, 3, 512, 526, 486);
+       //iterations,up,down
+       nod(3, 526, 486);
      } else if(c == 's'){
-       //delay,iterations,center,left,right
-       shake(20, 3, 512, 538, 486);
+       //iterations,left,right
+       shake(3, 538, 486);
      } else if (c == 'c'){
-       if(d == 'r'){
-         center(20, 1, 486);
-       } else if(d == 'l'){
-         center(20, 1, 538);
-       } else if(d == 'u'){
-         center(20, 2, 538);
-       } else {
-         center(20, 2, 486);
-       }
+       center();
        d = 'c';
      } else if (c == 'r'){
-       down_right(20, 1, 512, 486);
+       engine1 = down_right(1, 486);
        d = 'r';
      } else if (c == 'l'){
-       up_left(20, 1, 512, 538);
+       engine1 = up_left(1, 538);
        d = 'l';
      } else if (c == 'u'){
-       up_left(20, 2, 512, 538);
+       engine2 = up_left(2, 538);
        d = 'u';
      } else if (c == 'd'){
-       down_right(20, 2, 512, 486);
+       engine2 = down_right(2, 486);
        d = 'd';
      } else if (c == 'a'){
        anger();
@@ -53,51 +47,65 @@ void loop()
  }
  
 // 1 step = 0.29º
-int nod(int s, int t, int ini, int top, int down){
-  up_left(s, 2, ini, top);
+int nod(int t, int top, int down){
+  int temp = engine2;
+  engine2 = up_left(2, top);
   for(int i = 0; i < t; i++){
-    down_right(s, 2, top, down);
-    up_left(s, 2, down, top);
+    engine2 = down_right(2, down);
+    engine2 = up_left(2, top);
   }
-  down_right(s, 2, top, ini);
+  engine2 = down_right(2, temp);
 }
 
-int shake(int s, int t, int ini, int top, int down){
-  down_right(s, 1, ini, down);
+int shake(int t, int top, int down){
+  int temp = engine2;
+  engine1 = down_right(1, down);
   for(int i = 0; i < t; i++){
-    up_left(s, 1, down, top);  
-    down_right(s, 1, top, down);
+    engine1 = up_left(1, top);  
+    engine1 = down_right(1, down);
   }
-  up_left(s, 1, down, ini);
+  engine1 = up_left(1, temp);
 }
 
-int up_left(int s, int engine, int ini, int top){
-  for(int j = ini; j < top; j++){
-      SetPosition(engine, j);
-      delay(s);
+int up_left(int engine, int top){
+  int j = (engine == 1 ? engine1 : engine2);
+  while(j != top){
+    j++;
+    SetPosition(engine, j);
+    delay(s);
+  }
+  return j;
+}
+
+int down_right(int engine, int top){
+  int j = (engine == 1 ? engine1 : engine2);
+  while(j != top){
+    j--;
+    SetPosition(engine, j);
+    delay(s);
+  }
+  return j;
+}
+
+void center(){
+  while(engine1 != 512 || engine2 != 512){
+    if(engine1 != 512){
+      if(engine1 > 512){ engine1--; } else { engine1++; }
+      SetPosition(1, engine1);
     }
-}
-
-int down_right(int s, int engine, int ini, int top){
-  for(int j = ini; j > top; j--){
-      SetPosition(engine, j);
-      delay(s);
+    if(engine2 != 512){
+      if(engine2 > 512){ engine2--; } else { engine2++; }
+      SetPosition(2, engine2);
     }
-}
-
-int center(int s, int engine, int from){
-  if(from > 512){
-    down_right(s, engine, from, 511);
-  } else {
-    up_left(s, engine, from, 513);
+    delay(s);
   }
 }
 
 int anger(){
-  down_right(20, 2, 512, 486);
-  down_right(20, 1, 512, 456);
-  shake(20, 3, 456, 492, 440);
-  up_left(20, 1, 456, 513);
+  engine2 = down_right(2, 486);
+  engine1 = down_right(1, 456);
+  shake(3, 492, 440);
+  engine1 = up_left(1, 512);
 }
 
 
