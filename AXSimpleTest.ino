c5840657cc3e4eac3a33e9434d79b97f839360d5
Adrian Acosta Mitjans <adrian.mitjans@gmail.com>
2021-04-19 23:09:00 -0700

    Eliminados los limites

--- AXSimpleTest.ino
+++ AXSimpleTest.ino
@@ -18,30 +18,32 @@ void loop()
    if (Serial.available() > 0) {
      // Get next command from Serial (add 1 for final 0)
      char c = Serial.read();
-     if(c == 'n' || c == 'N'){
-       //iterations,up,down
-       nod(3, (c == 'N' ? 526 : ((512 + 526) / 2)), (c == 'N' ? 486 : ((512 + 486) / 2)));
-     } else if(c == 's' || c == 'S'){
-       //iterations,left,right
-       shake(3, (c == 'S' ? 538 : ((512 + 538) / 2)), (c == 'S' ? 486 : ((512 + 486) / 2)));
-     } else if (c == 'c'){
+     if (c == 'c'){
        center();
        d = 'c';
      } else if (c == 'r' || c == 'R'){
-       engine1 = down_right(1, c == 'R' ? 486 : ((512 + 486) / 2));
+       engine1 = down_right(1, c == 'R' ? 26 : 13);
        d = 'r';
      } else if (c == 'l' || c == 'L'){
-       engine1 = up_left(1, c == 'L' ? 538 : ((512 + 538) / 2));
+       engine1 = up_left(1, c == 'L' ? 26 : 13);
        d = 'l';
      } else if (c == 'u' || c == 'U'){
-       engine2 = up_left(2, c == 'U' ? 538 : ((512 + 538) / 2));
+       engine2 = up_left(2, c == 'U' ? 26 : 13);
        d = 'u';
      } else if (c == 'd' || c == 'D'){
-       engine2 = down_right(2, c == 'D' ? 486 : ((512 + 486) / 2));
+       engine2 = down_right(2, c == 'D' ? 26 : 13);
        d = 'd';
-     } else if (c == 'a'){
-       anger();
-       d = 'd';
+     } else if ( c == 'h') {
+       String codes = "[{\"code\":\"c\",\"symbol\":\"&olarr;\"},";
+       codes = codes + "{\"code\":\"r\",\"symbol\":\"&rarr;\"},";
+       codes = codes + "{\"code\":\"l\",\"symbol\":\"&larr;\"},";
+       codes = codes + "{\"code\":\"u\",\"symbol\":\"&uarr;\"},";
+       codes = codes + "{\"code\":\"d\",\"symbol\":\"&darr;\"},";
+       codes = codes + "{\"code\":\"R\",\"symbol\":\"&rrarr;\"},";
+       codes = codes + "{\"code\":\"L\",\"symbol\":\"&llarr;\"},";
+       codes = codes + "{\"code\":\"U\",\"symbol\":\"&uuarr;\"},";
+       codes = codes + "{\"code\":\"D\",\"symbol\":\"&ddarr;\"}]";
+       Serial.println(codes);
      } else {
        onestep(c);
      }
@@ -49,28 +51,9 @@ void loop()
  }
  
 // 1 step = 0.29º
-int nod(int t, int top, int down){
-  int temp = engine2;
-  engine2 = up_left(2, top);
-  for(int i = 0; i < t; i++){
-    engine2 = down_right(2, down);
-    engine2 = up_left(2, top);
-  }
-  engine2 = down_right(2, temp);
-}
-
-int shake(int t, int top, int down){
-  int temp = engine1;
-  engine1 = down_right(1, down);
-  for(int i = 0; i < t; i++){
-    engine1 = up_left(1, top);  
-    engine1 = down_right(1, down);
-  }
-  engine1 = up_left(1, temp);
-}
-
 int up_left(int engine, int top){
   int j = (engine == 1 ? engine1 : engine2);
+  top = top + j;
   while(j != top){
     j++;
     SetPosition(engine, j);
@@ -81,6 +64,7 @@ int up_left(int engine, int top){
 
 int down_right(int engine, int top){
   int j = (engine == 1 ? engine1 : engine2);
+  top = j - top;
   while(j != top){
     j--;
     SetPosition(engine, j);
@@ -103,13 +87,6 @@ void center(){
   }
 }
 
-int anger(){
-  engine2 = down_right(2, 486);
-  engine1 = down_right(1, 456);
-  shake(3, 492, 440);
-  engine1 = up_left(1, 512);
-}
-
 void onestep(char c){
   if(c == 't'){
     engine2++;
@@ -123,6 +100,3 @@ void onestep(char c){
   SetPosition(1, engine1);
   SetPosition(2, engine2);
 }
-
-
-
